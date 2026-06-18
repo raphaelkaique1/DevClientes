0b66e89859d926a136509213ed2cff03241feed7
raphaelkaique1 <raphaelkaiquediassantos1@gmail.com>
2026-06-18 14:19:11 -0300

    refactor: adds a robust control structure to app router

--- App.php
+++ App.php
@@ -1,11 +1,17 @@
 <?php
-declare(strict_type=1);
+declare(strict_types=1);
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-if($path !== '/' && is_file(__DIR__ . $path)) return false;
-if(str_starts_with($path, '/api/')) {
-    require __DIR__ . '/api/index.php';
-    return true;
-}
-require __DIR__ . '/public/index.html';
+
+return match (true) {
+    $path !== '/' && is_file(__DIR__ . $path) => false,
+    str_starts_with($path, '/api/') => (function () {
+        require __DIR__ . '/api/index.php';
+        return true;
+    })(),
+    default => (function () {
+        require __DIR__ . '/public/index.html';
+        return null;
+    })(),
+};
 
 ?>

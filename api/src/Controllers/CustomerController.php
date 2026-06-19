ce5756a7c14277a7236da81dd29436ac8685a306
raphaelkaique1 <raphaelkaiquediassantos1@gmail.com>
2026-06-19 06:04:14 -0300

    feat: add SQLSTATE error validation ([Customer.email] constraint UNIQUE violation)

--- api/src/Controllers/CustomerController.php
+++ api/src/Controllers/CustomerController.php
@@ -29,9 +29,13 @@ class CustomerController {
             (new Response(ContentType::TEXT, 'Preencha todos os campos obrigatórios!', 406))->send();
             return;
         }
-        match(Operation::runSafe(fn() => (new CustomerService())->create($data))) {
-            true    => (new Response(ContentType::TEXT, 'Usuário criado com sucesso!', 201))->send(),
-            false   => (new Response(ContentType::TEXT, 'Não foi possível criar o usuário.', 400))->send(),
+        match($result = Operation::runSafe(fn() => (new CustomerService())->create($data)) and true) {
+            $result === true  => (new Response(ContentType::TEXT, 'Usuário criado com sucesso!', 201))->send(),
+            $result === false => (new Response(ContentType::TEXT, 'Não foi possível criar o usuário.', 400))->send(),
+            str_contains(
+                $result,
+                Violation::DuplicateEmailException->value
+            )       => (new Response(ContentType::TEXT, 'Usuário já possui cadastro!', 409))->send(),
             default => (new Response(ContentType::TEXT, 'Erro ao cadastrar usuário!', 500))->send()
         };
     }
--- api/src/Utils/Exception.php
+++ api/src/Utils/Exception.php
@@ -1,5 +1,10 @@
 <?php
 declare(strict_types=1);
+
+enum Violation: string {
+    case DuplicateEmailException = '23000';
+}
+
 class Operation {
     public static function runSafe(callable $callback): mixed {
         try {

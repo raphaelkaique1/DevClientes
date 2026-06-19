0bc9fd1ffdefa6bc73275e6eeabf6564fc3322e6
raphaelkaique1 <raphaelkaiquediassantos1@gmail.com>
2026-06-19 06:54:44 -0300

    refactor(frontend): remove JS dep in UI

--- public/js/script.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-document.body.addEventListener('htmx:afterSwap', event => {
-    if(event.detail.requestConfig.verb !== 'get') setTimeout(() => document.getElementById(event.target.id).innerText = '', 4000);
-});
